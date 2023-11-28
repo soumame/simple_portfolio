360adfbc7b4775f51a80050a41a1fcd8a864a338
soumame <sou.tokumaru@gmail.com>
2023-11-28 21:08:35 +0800

    Lucideを追加 Update Astro configuration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import nodejs from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  adapter: vercel(),
+  output: "hybrid"
 });
