db68f1eb1846e87dc87cc28bc402bcb260b54ba9
soumame <sou.tokumaru@gmail.com>
2023-12-05 08:31:30 +0800

    ビルドコマンドを変更@package json、translatorのconsole.log変更

--- src/works/translator.cjs
+++ src/works/translator.cjs
@@ -31,7 +31,7 @@ async function translateText(text, targetLanguage) {
       temperature: 0.7,
       max_tokens: 1000,
     });
-    console.log(`worksの内容(${text})を翻訳中...`);
+    console.log(`翻訳中...(WORKS)(${text})`);
     return response;
   } catch (error) {
     console.error("翻訳中にエラーが発生したよ:", error);
