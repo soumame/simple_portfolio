d3d362c4b202fd6b430a1022fc0a6ee3a86c955e
soumame <sou.tokumaru@gmail.com>
2023-12-11 13:36:37 +0800

    レンダリングモードを静的に変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import nodejs from "@astrojs/node";
-
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
-  output: "hybrid",
   integrations: [tailwind(), react()],
   experimental: {
     i18n: {
       defaultLocale: "ja",
       locales: ["en", "ja"],
       routing: {
-        prefixDefaultLocale: true
-      }
-    }
-  }
+        prefixDefaultLocale: true,
+      },
+    },
+  },
 });
