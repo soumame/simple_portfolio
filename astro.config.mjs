1b272a997cedc0b448f50cb3aed722969910e124
soumame <sou.tokumaru@gmail.com>
2023-12-29 14:02:32 +0800

    タイムラインのデザイン変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import nodejs from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: nodejs({
+    mode: "middleware", // または'standalone'
+  }),
+  output: "hybrid",
   integrations: [tailwind(), react()],
   experimental: {
     i18n: {
