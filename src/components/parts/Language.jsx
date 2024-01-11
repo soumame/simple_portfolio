0572d0259bbb21505670014b99da00c86d3a85a5
soumame <sou.tokumaru@gmail.com>
2024-01-11 23:59:11 +0800

    赤字のところを治しといたからこれで動くはず

--- src/components/parts/Language.jsx
+++ src/components/parts/Language.jsx
@@ -22,10 +22,13 @@ const LanguagePicker = () => {
           className="min-w-[220px] bg-gray-400/30 backdrop-blur-md rounded-2xl p-[5px] shadow-md border border-gray-500/10 will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
           sideOffset={5}
         >
-          {Object.entries(languages).map(([lang, label]) => (
-            <DropdownMenu.Item className=" group text-md p-6 leading-none text-gray-700 rounded-2xl flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-gray-600 data-[disabled]:pointer-events-none data-[highlighted]:bg-gray-300 data-[highlighted]:text-gray-600">
+          {Object.entries(languages).map(([lang, label, index]) => (
+            <DropdownMenu.Item
+              key={index}
+              className=" group text-md p-6 leading-none text-gray-700 rounded-2xl flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-gray-600 data-[disabled]:pointer-events-none data-[highlighted]:bg-gray-300 data-[highlighted]:text-gray-600"
+            >
               <a
-                className="absolute flex justify-cente items-center font-semibold  w-full h-full"
+                className="absolute flex items-center font-semibold  w-full h-full"
                 href={`/${lang}/`}
               >
                 {label}
--- src/components/parts/metadata.jsx
+++ src/components/parts/metadata.jsx
@@ -11,7 +11,8 @@ export default function Metadata(props) {
   const jsonData = JSON.parse(rawData);
   const SNSData = JSON.parse(rawSNS);
 
-  
+  const janame = jsonData.ja[0].name;
+  const enname = jsonData.ja[0].name;
   const jabio = jsonData.ja[0].bio;
   const enbio = jsonData.en[0].bio;
   const jaicon = jsonData.ja[0].icon;
@@ -19,23 +20,23 @@ export default function Metadata(props) {
 
   const sitePath = props.path;
 
-  const twitterLink = SNSData.find(item => item.title === 'Twitter')?.link;
-  const twitterUsername = twitterLink.split('/').pop();
+  const twitterLink = SNSData.find((item) => item.title === "Twitter")?.link;
+  const twitterUsername = twitterLink.split("/").pop();
 
+  const bioo = props.lang + "bio";
+  const bio = eval(bioo);
+  const iconn = props.lang + "icon";
+  const icon = eval(iconn);
+  const namee = props.lang + "name";
+  const name = eval(namee);
 
-
-    const bioo = props.lang + 'bio'
-        const bio = eval(bioo);
-    const iconn = props.lang + 'icon'
-        const icon = eval(iconn);
-
-return (
+  return (
     <head>
       {" "}
       <meta name="description" content={bio} />
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width" />
-      <title>AstroOoooooOOoo</title>
+      <title>{name}</title>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link rel="icon" href={icon}></link>
@@ -43,13 +44,11 @@ return (
         href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@1,300;1,700&family=Noto+Sans+JP:wght@200;400;700&family=Zen+Kaku+Gothic+Antique:wght@300&display=swap"
         rel="stylesheet"
       />
-      <meta name="twitter:card" content="summary"/>
-      <meta name="twitter:site" content={twitterUsername}/>
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content={twitterUsername} />
       <meta name="twitter:creator" content={twitterUsername} />
-      <meta name="twitter:image" content={icon}/>
-
+      <meta name="twitter:image" content={icon} />
       <meta property="og:url" content={sitePath} />
-
-    </head>,
+    </head>
   );
 }
