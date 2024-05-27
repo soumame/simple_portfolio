5e0a12e548d8baa351713f4264c2c73250e3f815
soumame <sou.tokumaru@gmail.com>
2024-05-27 19:08:31 +0800

    update site url of config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  site: "https://so-bean.work",
   i18n: {
     defaultLocale: "ja",
     locales: ["en", "ja"],
