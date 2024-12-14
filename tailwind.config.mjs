d045392499a60230f8ac60c0d232327cd161ba7e
soumame <sou.tokumaru@gmail.com>
2024-12-14 11:55:24 +0800

    フォントスタイルを変更し、ボックスシャドウの設定を追加

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,12 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      boxShadow: {
+        md: "5px 5px 6px rgb(0 0 0 / 0.05), -5px -5px 6px rgb(255 255 255 / 0.2);",
+        lg: "12px 12px 10px rgb(0 0 0 / 0.05), -12px -12px 10px rgb(255 255 255 / 0.2);",
+        inner:
+          "inset 5px 5px 6px rgb(50 50 50 / 0.0), inset -5px -5px 6px rgb(255 255 255 / 0.1);",
+      },
       keyframes: {
         slideDownAndFade: {
           from: { opacity: 0, transform: "translateY(-2px)" },
@@ -37,8 +43,8 @@ export default {
         "Zen Kaku Gothic Antique",
         "Noto Sans Japanese",
       ],
+      serif: ['"IBM Plex Serif"', "Noto Serif JP"],
       noto: ["Noto Sans Japanese 200"],
-      mame: ["Mamefont"],
     },
   },
   plugins: [
